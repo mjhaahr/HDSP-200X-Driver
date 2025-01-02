69f7b164f8c8ff7b3fd84da5dc01898b87769579
Matthew Haahr <matthew.haahr@gmail.com>
2025-01-01 23:56:27 -0800

    [HDSP-200X 3] Clean up a bit and add some more docs

--- HDSP_200X.cpp
+++ HDSP_200X.cpp
@@ -1,4 +1,4 @@
-/***** HDSP-200x Driver ***** 
+/***** HDSP-200x Driver *****
 
 Written By Matthew Haahr
 
@@ -15,121 +15,122 @@ char HDSP_200X::chars[(4 * MAX_DISPLAYS) + 1][5] = {0};
 
 
 HDSP_200X::HDSP_200X(char* columns, char data, char clock, unsigned char num) {
-  for (int i = 0; i < NUM_COLS; i++) {
-    this->column[i] = columns[i];
-  }
-  this->data = data;
-  this->clock = clock;
-  this->num = num;
-  
-  // Pull everything low at start to prevent floating pins and latch the shift registers 
-  for (int i = 0; i < NUM_COLS; i++) {
-    pinMode(this->column[i], OUTPUT);
-    digitalWrite(this->column[i], LOW);
-  }
-  pinMode(this->data, OUTPUT);
-  pinMode(this->clock, OUTPUT);
-  digitalWrite(this->data, LOW);
-  digitalWrite(this->clock, LOW);
-  
-  //thisDisplay = this;
+    for (int i = 0; i < NUM_COLS; i++) {
+        this->column[i] = columns[i];
+    }
+    this->data = data;
+    this->clock = clock;
+    this->num = num;
+
+    // Pull everything low at start to prevent floating pins and latch the shift registers
+    for (int i = 0; i < NUM_COLS; i++) {
+        pinMode(this->column[i], OUTPUT);
+        digitalWrite(this->column[i], LOW);
+    }
+    pinMode(this->data, OUTPUT);
+    pinMode(this->clock, OUTPUT);
+    digitalWrite(this->data, LOW);
+    digitalWrite(this->clock, LOW);
+
+    //thisDisplay = this;
 }
 
 // TODO: test without latch time
 void HDSP_200X::testDisplay(char num) {
-  for (int i = 0; i < (7 * 4 * num); i++) {
-    digitalWrite(clock, HIGH); // Latch clock
-    digitalWrite(data, LOW); // shift out a 0 clear display
-    digitalWrite(clock, LOW); // end clock pulse
-  }
-  
-  // First one needs to be handled seperately
-  digitalWrite(clock, HIGH); // Latch clock
-  digitalWrite(data, HIGH); // shift out a 1 (start it off with a one)
-  digitalWrite(clock, LOW); // end clock pulse
-  
-  for (int i = 0; i < (7 * 4 * num); i++) { // loops until clear
-    for (int k = 0; k < NUM_COLS; k++) { // k is column number
-      // directly addressing columns for void character control
-      digitalWrite(column[k], HIGH); 
-      delayMicroseconds(2000); 
-      digitalWrite(column[k], LOW); 
-      delayMicroseconds(1000); 
+    for (int i = 0; i < (7 * 4 * num); i++) {
+        digitalWrite(clock, HIGH); // Latch clock
+        digitalWrite(data, LOW); // shift out a 0 clear display
+        digitalWrite(clock, LOW); // end clock pulse
     }
+
+    // First one needs to be handled seperately
     digitalWrite(clock, HIGH); // Latch clock
-    digitalWrite(data, LOW); // shift out a 0
+    digitalWrite(data, HIGH); // shift out a 1 (start it off with a one)
     digitalWrite(clock, LOW); // end clock pulse
-  }
+
+    for (int i = 0; i < (7 * 4 * num); i++) { // loops until clear
+        for (int k = 0; k < NUM_COLS; k++) { // k is column number
+            // directly addressing columns for void character control
+            digitalWrite(column[k], HIGH);
+            delayMicroseconds(2000);
+            digitalWrite(column[k], LOW);
+            delayMicroseconds(1000);
+        }
+        digitalWrite(clock, HIGH); // Latch clock
+        digitalWrite(data, LOW); // shift out a 0
+        digitalWrite(clock, LOW); // end clock pulse
+    }
 }
-    
+
 void HDSP_200X::writeData(unsigned long out) {
-  out = out & 0x0FFFFFFF;  // mask to only 28 bits, shift register size
-  for (char pos = 0; pos < 28; pos++) { // bitbang data
-    digitalWrite(clock, HIGH); // Start clock
-    digitalWrite(data, (out >> pos) & 1); // write specific pixel
-    digitalWrite(clock, LOW); // end clock pulse
-  }
+    out = out & 0x0FFFFFFF;  // mask to only 28 bits, shift register size
+    for (char pos = 0; pos < 28; pos++) { // bitbang data
+        digitalWrite(clock, HIGH); // Start clock
+        digitalWrite(data, (out >> pos) & 1); // write specific pixel
+        digitalWrite(clock, LOW); // end clock pulse
+    }
 }
 
 void HDSP_200X::updateString(char *newString) {
-  len = strlen(newString);
-  this->updateString(newString, len);
+    len = strlen(newString);
+    this->updateString(newString, len);
 }
 
 void HDSP_200X::updateString(char *newString, unsigned int len) {
-  free(currentString);
-  this->len = len;
-  currentString = (char *) malloc(len + 1);
-  for (unsigned int i = 0; i < len + 1; i++) {
-    currentString[i] = 0;  //refill
-  }
-  for (unsigned int i = 0; i < len; i++) {
-    currentString[i] = newString[i];  //copy
-  }
+    free(currentString);
+    this->len = len;
+    currentString = (char *) malloc(len + 1);
+    for (unsigned int i = 0; i < len + 1; i++) {
+        currentString[i] = 0;  //refill
+    }
+    for (unsigned int i = 0; i < len; i++) {
+        currentString[i] = newString[i];  //copy
+    }
 }
 
 void HDSP_200X::clear(void) {
-  for (int i = 0; i < this->num; i++) {
-    writeData(0); // send all zeros (blank spaces)
-  }
+    for (int i = 0; i < this->num; i++) {
+        writeData(0); // send all zeros (blank spaces)
+    }
 }
 
 void HDSP_200X::pause(void) {
-  //ITimer1.pauseTimer();
+    //ITimer1.pauseTimer();
 }
 
 void HDSP_200X::draw(void) {
-  //ITimer1.resumeTimer();
+    //ITimer1.resumeTimer();
 }
 
 char *HDSP_200X::getCurrentString() {
-  return currentString;
+    return currentString;
 }
 
 void HDSP_200X::ISRHandle(void) {
-  //thisDisplay->displayUpdate();
+    //thisDisplay->displayUpdate();
 }
 
 void HDSP_200X::displayUpdate(void) {
-  for (int i = 0; i < 4 * len; i++) { // shift down (by ' ', 0x20) to match actual matrix map from chars (ignore control chars)
-    for (int j = 0; j < 5; j++) {
-      chars[i][j] = pgm_read_byte_near((char *)&char_data[(currentString[i] - ' ')][j]); 
-      // read from map, subtract ' ' to get to visible characters
-      // read from program memory
+    for (uint8_t i = 0; i < 4 * len; i++) { // shift down (by ' ', 0x20) to match actual matrix map from chars (ignore control chars)
+        uint8_t charOffset = (currentString[i] - ' ');
+        for (uint8_t j = 0; j < 5; j++) {
+            chars[i][j] = pgm_read_byte_near(&char_data[charOffset][j]);
+            // read from map, subtract ' ' to get to visible characters
+            // read from program memory
+        }
     }
-  }
-  for (int i = 0; i < 5; i++) {
-    for (int j = (num - 1); j >= 0; j--) { // write last 4 first, then write the displays used
-      // see above for shifting information
-      unsigned long out = 
-        (((unsigned long) chars[(4 * j) + 0][i]) << (3 * NUM_ROWS)) +
-        (((unsigned long) chars[(4 * j) + 1][i]) << (2 * NUM_ROWS)) +
-        (((unsigned long) chars[(4 * j) + 2][i]) << NUM_ROWS) +
-        ((unsigned long) chars[(4 * j) + 3][i]);
-      writeData(out);
+    for (uint8_t i = 0; i < 5; i++) {
+        for (uint8_t j = (num - 1); j >= 0; j--) { // write last 4 first, then write the displays used
+            // see above for shifting information
+            unsigned long out =
+            (((unsigned long) chars[(4 * j) + 0][i]) << (3 * NUM_ROWS)) +
+            (((unsigned long) chars[(4 * j) + 1][i]) << (2 * NUM_ROWS)) +
+            (((unsigned long) chars[(4 * j) + 2][i]) << NUM_ROWS) +
+            ((unsigned long) chars[(4 * j) + 3][i]);
+            writeData(out);
+        }
+        digitalWrite(column[i], HIGH);
+        delay(2);
+        digitalWrite(column[i], LOW);
     }
-    digitalWrite(column[i], HIGH); 
-    delay(2); 
-    digitalWrite(column[i], LOW);
-  }
 }
--- HDSP_200X.h
+++ HDSP_200X.h
@@ -1,14 +1,14 @@
-/***** HDSP-200x Driver ***** 
+/***** HDSP-200x Driver *****
 
 Written By Matthew Haahr
 
 Utilizes pgmspace.h and matrix.h for font mapping
 */
 
+#include "matrix.h"
 #include <Arduino.h>
 #include <avr/pgmspace.h>
 #include <string.h>
-#include "matrix.h"
 
 #ifndef HDSP_200X_H
 #define HDSP_200X_H
@@ -22,82 +22,82 @@ Utilizes pgmspace.h and matrix.h for font mapping
 * HDSP Display Class, contains all methods and data
 */
 class HDSP_200X {
-  public:
-    /**
-     * Constructor Method
-     * @param columns The column control pins, in order
-     * @param data The data pin
-     * @param clock The clock pin
-     * @param num The number of 4 character display units
-     */
-    HDSP_200X(char *columns, char data, char clock, unsigned char num);
-    
-    /**
-     * Updates the string stored in the memory (frees and reallocates)
-     * @param newString Pointer ton the new string
-     */
-    void updateString(char *newString);
+    public:
+        /**
+         * Constructor Method
+         * @param columns The column control pins, in order
+         * @param data The data pin
+         * @param clock The clock pin
+         * @param num The number of 4 character display units
+         */
+        HDSP_200X(char *columns, char data, char clock, unsigned char num);
 
-    /**
-     * Updates the string stored in mem
-     * @Overload
-     * @param newString newString pointer
-     * @param len The length of the string
-     */
-    void updateString(char *newString, unsigned int len);
-    
-    /**
-     * Clears the displays, rewrites all data to zero
-     */
-    void clear(void);
-    
-    /**
-     * Pauses the display (stops the update calls)
-     */
-    void pause(void);
+        /**
+         * Updates the string stored in the memory (frees and reallocates)
+         * @param newString Pointer ton the new string
+         */
+        void updateString(char *newString);
 
-    /**
-     * Restarts the display (continues the update calls)
-     */
-    void draw(void);
-    
-    /**
-     * Scrolls a dot down each pixel in the number of displays used
-     * @param num =1 The number of 4 character display the dot should travel along, default is one display
-     */
-    void testDisplay(char num = 1);
+        /**
+         * Updates the string stored in mem
+         * @Overload
+         * @param newString newString pointer
+         * @param len The length of the string
+         */
+        void updateString(char *newString, unsigned int len);
 
-    /**
-     * Gets the current text
-     * @return The current text displayed on the display as a char*
-     */
-    char *getCurrentString(void);
-    
-     /**
-      * The actual ISR handler, non-static method
-      */
-     void displayUpdate(void);
-  
-  private:
-    char column[5];  // The column pins
-    char data;  // The data pin
-    char clock;  // The clock pin
-    unsigned char num; // The number of display units
-    char *currentString;  // The current string being displayed
-    unsigned char len;  // The length of currentString
-    static char chars[(4 * MAX_DISPLAYS) + 1][5];
+        /**
+         * Clears the displays, rewrites all data to zero
+         */
+        void clear(void);
 
-    /**
-     * Writes a 28 bit stream to the displays shift registers
-     * @param the data to shift out
-     */
-    void writeData(unsigned long out);
-    
-    /**
-     * Timer ISR for display updaing
-     */
-     static void ISRHandle(void);
+        /**
+         * Pauses the display (stops the update calls)
+         */
+        void pause(void);
 
+        /**
+         * Restarts the display (continues the update calls)
+         */
+        void draw(void);
+
+        /**
+         * Scrolls a dot down each pixel in the number of displays used
+         * @param num =1 The number of 4 character display the dot should travel
+         * along, default is one display
+         */
+        void testDisplay(char num = 1);
+
+        /**
+         * Gets the current text
+         * @return The current text displayed on the display as a char*
+         */
+        char *getCurrentString(void);
+
+        /**
+         * The actual ISR handler, non-static method
+         */
+        void displayUpdate(void);
+
+    private:
+        char column[5];      // The column pins
+        char data;           // The data pin
+        char clock;          // The clock pin
+        unsigned char num;   // The number of display units
+        char *currentString; // The current string being displayed
+        unsigned char len;   // The length of currentString
+        static char chars[(4 * MAX_DISPLAYS) + 1][5];
+
+        /**
+         * Writes a 28 bit stream to the displays shift registers
+         * @param the data to shift out
+         */
+        void writeData(unsigned long out);
+
+        /**
+         * Timer ISR for display updaing
+         */
+        static void ISRHandle(void);
 };
 
 #endif
